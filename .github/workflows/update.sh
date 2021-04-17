b055fd8da916160b5aedc50d447da0ff0bee2cae
Soh Satoh <20023945+sohsatoh@users.noreply.github.com>
2021-04-18 01:00:23 +0900

    Test

---
+++ .github/workflows/update.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+mkdir test
+cd test
+
+curl -O "https://repo.theodyssey.dev/otas/taurine-base.json"
+VERSION=$(cat taurine-base.json | jq -r '.name')
+URL=$(cat taurine-base.json | jq -r '.download')
+
+echo ${VERSION}
+echo ${URL}
+
+mkdir files
+curl ${URL} | tar zx -C files
+
+cd files
+
+CONTENTS=""
+LF=$'\n'
+
+while read -d $'\0' file; do
+  # CONTENTS = NULL
+  file=$(echo $file | sed -e "s/.\///g")
+  hash=($(shasum -a 256 ${file}))
+  echo ${hash}
+  CONTENTS+="\"${file}\": \"${hash}\",${LF}    "
+done < <(find . -mindepth 1 -maxdepth 1 -print0)
+
+printf '{
+  "name": %s,
+  "sha256":[
+    %s
+  ]
+}' "${VERSION}" "${CONTENTS}"
+
+cd ../..
+rm -rf test
+
+pwd
