bb73e6766b50e15064379b9b9cd237c9390d1144
Soh Satoh <20023945+sohsatoh@users.noreply.github.com>
2021-04-18 02:25:27 +0900

    Update

--- update.sh
+++ update.sh
@@ -4,7 +4,7 @@ mkdir test
 cd test
 
 curl -O "https://repo.theodyssey.dev/otas/taurine-base.json"
-VERSION=$(cat taurine-base.json | jq -r '.name')
+VERSION=$(cat taurine-base.json | jq -r '.name' | sed -e "s/ \[.*\]//")
 URL=$(cat taurine-base.json | jq -r '.download')
 
 echo ${VERSION}
@@ -20,7 +20,7 @@ LF=$'\n'
 
 while read -d $'\0' file; do
   # CONTENTS = NULL
-  file=$(echo $file | sed -e "s/.\///g")
+  file=$(echo $file | sed -e "s/\.\///")
   hash=($(shasum -a 256 ${file}))
   echo ${hash}
   CONTENTS+="\"${file}\": \"${hash}\",${LF}    "
